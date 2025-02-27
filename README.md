51f603bca4251c6c5e76b924b67d4053ee9e06a8
RenHuangfu <1225836449@qq.com>
2025-02-27 11:56:34 +0800

    readme

---
+++ README.md
@@ -0,0 +1,9 @@
+构建docker镜像
+./deploy/build.sh
+
+运行docker容器
+./deploy/run.sh
+
+端口
+业务系统接 8000
+客户端接 9001
--- deploy/build.sh
+++ deploy/build.sh
@@ -1,17 +1,17 @@
 # 构建 manager 服务
 docker build \
-  -f ../app/manager/Dockerfile \
+  -f app/manager/Dockerfile \
   -t manager-service:latest \
   .
 
 # 构建 pusher 服务
 docker build \
-  -f ../app/pusher/Dockerfile \
+  -f app/pusher/Dockerfile \
   -t pusher-service:latest \
   .
 
 # 构建 receiver 服务
 docker build \
-  -f ../app/receiver/Dockerfile \
+  -f app/receiver/Dockerfile \
   -t receiver-service:latest \
   .
